87da746795cb214205df53525bf61e642b3b3560
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2025-08-15 13:23:23 +0530

    created RebtalRepository class

--- Backend/src/main/java/com/example/Book_Rental_Management_System/Repository/RentalRepository.java
+++ Backend/src/main/java/com/example/Book_Rental_Management_System/Repository/RentalRepository.java
@@ -1,4 +1,9 @@
 package com.example.Book_Rental_Management_System.Repository;
 
-public class RentalRepository {
+import com.example.Book_Rental_Management_System.Model.Rental;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RentalRepository extends JpaRepository<Rental, Long> {
 }
