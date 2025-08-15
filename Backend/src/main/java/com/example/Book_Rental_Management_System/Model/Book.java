c741040ed951c951169544f8fb288dfa74fbf261
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2025-08-15 13:13:32 +0530

    created book class

--- Backend/src/main/java/com/example/Book_Rental_Management_System/Model/Book.java
+++ Backend/src/main/java/com/example/Book_Rental_Management_System/Model/Book.java
@@ -1,4 +1,31 @@
 package com.example.Book_Rental_Management_System.Model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name ="Book")
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    @Column(name = "title",nullable = false)
+    private String title;
+
+    @Column(name = "author",nullable = false)
+    private String author;
+
+    @Column(name = "genre",nullable = false)
+    private String genre;
+
+    @Column(name = "available",nullable = false)
+    private Boolean available = true;
 }
+
