b726a0657f32d4ab793cafc8682ed2fc91d7560e
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2025-08-15 13:22:38 +0530

    created BookRepository interface

--- Backend/src/main/java/com/example/Book_Rental_Management_System/Repository/BookRepository.java
+++ Backend/src/main/java/com/example/Book_Rental_Management_System/Repository/BookRepository.java
@@ -1,4 +1,9 @@
 package com.example.Book_Rental_Management_System.Repository;
 
-public class BookRepository {
+import com.example.Book_Rental_Management_System.Model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
 }
