911e74c27933c8c0268da3cbb561272fccd295fc
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2025-08-15 12:21:50 +0530

    Initialized the project

---
+++ Backend/src/main/java/com/example/Book_Rental_Management_System/BookRentalManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Book_Rental_Management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRentalManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookRentalManagementSystemApplication.class, args);
+	}
+
+}
